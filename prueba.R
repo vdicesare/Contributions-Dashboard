5d58a247597d19692811cf18284ab672b595ba98
VictoriaDiCesare <79263847+VictoriaDiCesare@users.noreply.github.com>
2023-01-23 16:45:10 +0100

    Add mayor changes in order to include new widget

--- prueba.R
+++ prueba.R
@@ -4,26 +4,32 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 
-# Data import
-df <- fread ('https://zenodo.org/record/3891055/files/plos_contribution_data_set.csv?download=1',
-             select = c("wrote_paper", "analyzed_data", "conceived_experiments", "contributed_tools", "performed_experiments", "p_age"))
+# Import data
+df <- fread ('https://zenodo.org/record/3891055/files/plos_contribution_data_set.csv?download=1')
 
-# Data ordering by age
-dfOrdByAge <- df[order(df[,6]),]
+# Create au_position variable
+df$au_position <- ifelse(df$au_count == 1, "1st author",
+                         ifelse(df$au_count > 1 & df$au_count != df$n_authors, "2nd/middle author",
+                                ifelse(df$au_count == df$n_authors, "Last author", NA)
+                                )
+                         )
 
-# Data addition by age
-dfAddByAge <- aggregate(cbind(wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments) ~ p_age,
-                        data = dfOrdByAge, FUN = sum)
+# Convert au_position variable to factor
+df$au_position <- as.factor(df$au_position)
 
-# Data cleaning
-dfByAgeClean <- dfAddByAge[-c(1, 50, 51, 52),]
+# Remove n_authors = 1 and p_age outliers
+df_filtered <- filter(df, n_authors > 1 & p_age > 0 & p_age < 50)
 
-### Ggplot trial
-gather(data = dfByAgeClean, key, value, wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments) %>%
-  ggplot(aes(x = p_age, y = value, colour = key)) +
-  geom_line()
-###
+# Group contributions by p_age
+df_grouped_contributions <- aggregate(cbind(wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments) ~ p_age,
+                        data = df_filtered, FUN = sum)
 
+# Group au_position by p_age
+df_grouped_positions <- df_filtered %>% group_by(p_age, au_position) %>% 
+  summarise(cases = n(),.groups = 'drop') %>%
+  as.data.frame()
+
+### APP
 # User interface
 ui <- fluidPage(
   
@@ -51,7 +57,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$contributionByAge <- renderPlot({
-    gather(data = dfByAgeClean[which(dfByAgeClean$p_age >= input$age[1] & dfByAgeClean$p_age <= input$age[2]),], key, value, wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments) %>%
+    gather(data = df_grouped_contributions[which(df_grouped_contributions$p_age >= input$age[1] & df_grouped_contributions$p_age <= input$age[2]),], key, value, wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments) %>%
       
       {if (!input$contribution_WR) filter(., key!='wrote_paper') else filter(.)} %>%
       {if (!input$contribution_AD) filter(., key!='analyzed_data') else filter(.)} %>%
@@ -64,5 +70,12 @@ server <- function(input, output) {
   })
 }
 
-# App run
+# Run app
 shinyApp(ui = ui, server = server)
+
+### Add authors' positions widget
+
+gather(data = dfByAgeClean, key, value, wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments) %>%
+  ggplot(aes(x = p_age, y = value, colour = key)) +
+  geom_line()
+###
