b801958285cacb3f1306b095056ef979cdce9ed8
VictoriaDiCesare <79263847+VictoriaDiCesare@users.noreply.github.com>
2023-01-27 12:04:27 +0100

    Create second tab panel with plot

--- prueba.R
+++ prueba.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinythemes)
 library(data.table)
 library(ggplot2)
 library(dplyr)
@@ -18,20 +19,29 @@ df$au_last <- ifelse(df$au_count == df$n_authors, 1, 0)
 df_filtered <- filter(df, n_authors > 1 & p_age > 0 & p_age < 50)
 
 # Group contributions and positions by p_age
-df_grouped <- aggregate(cbind(wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments, au_first, au_middle, au_last) ~ p_age,
-                        data = df_filtered, FUN = sum)
+df_grouped_age <- aggregate(cbind(wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments, au_first, au_middle, au_last) ~ p_age,
+                            data = df_filtered, FUN = sum)
+
+# Group contributions and positions by n_authors
+df_grouped_number_authors <- aggregate(cbind(wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments, au_first, au_middle, au_last) ~ n_authors,
+                                       data = df_filtered, FUN = sum)
 
 
 ### APP
 # User interface
-ui <- fluidPage(
+ui <- navbarPage(
   
-  titlePanel("Contributions Dashboard"),
+  # Application title
+  theme = shinytheme("paper"),
+  title="Contributions Types in Science",
   
+  # Tab panels
+  tabPanel("contribution_position_age",
+  h2("Contribution type and author position by academic age"),
   sidebarLayout(
     sidebarPanel(
       h5("Academic age"),
-      sliderInput("age", min = 1, max = 25, value = c(10, 15)),
+      sliderInput("age", label = "prueba", min = 1, max = 25, value = c(10, 15)),
       h5("Contribution type"),
       checkboxInput("contribution_WR", label = "Wrote paper", value = TRUE),
       checkboxInput("contribution_AD", label = "Analyzed data", value = TRUE),
@@ -44,17 +54,43 @@ ui <- fluidPage(
       checkboxInput("position_last", label = "Last author", value = TRUE)),
     
     mainPanel(
-      
-      plotOutput("contributions_dashboard")
+      plotOutput("contribution_position_age")
+      )
     )
-  )
+  ),
+
+  tabPanel("contribution_position_number_authors",
+  h2("Contribution type and author position by number of authors"),
+  sidebarLayout(
+    sidebarPanel(
+      h5("Number of authors"),
+      sliderInput("number_authors", label = "prueba", min = 2, max = 93, value = c(20, 50)),
+      h5("Contribution type"),
+      checkboxInput("contribution_WR", label = "Wrote paper", value = TRUE),
+      checkboxInput("contribution_AD", label = "Analyzed data", value = TRUE),
+      checkboxInput("contribution_CE", label = "Conceived experiments", value = TRUE),
+      checkboxInput("contribution_CT", label = "Contributed tools", value = TRUE),
+      checkboxInput("contribution_PE", label = "Performed experiments", value = TRUE),
+      h5("Author position"),
+      checkboxInput("position_first", label = "1st author", value = TRUE),
+      checkboxInput("position_middle", label = "2nd/middle author", value = TRUE),
+      checkboxInput("position_last", label = "Last author", value = TRUE)),
+             
+    mainPanel(
+      plotOutput("contribution_position_number_authors")
+      )
+    )
+  )#,
+  
+  #ABOUT PANEL
+
 )
 
 # Server logic
 server <- function(input, output) {
   
-  output$contributions_dashboard <- renderPlot({
-    gather(data = df_grouped[which(df_grouped$p_age >= input$age[1] & df_grouped$p_age <= input$age[2]),],
+  output$contribution_position_age <- renderPlot({
+    gather(data = df_grouped_age[which(df_grouped_age$p_age >= input$age[1] & df_grouped_age$p_age <= input$age[2]),],
            key, value, wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments, au_first, au_middle, au_last) %>%
       
       {if (!input$contribution_WR) filter(., key!='wrote_paper') else filter(.)} %>%
@@ -68,7 +104,33 @@ server <- function(input, output) {
       
       ggplot(aes(x = p_age, y = value, colour = key, linetype = key)) +
       geom_line() +
+      theme_minimal() +
       xlab("Age") +
+      ylab("Count") +
+      scale_colour_discrete("References", breaks = c("wrote_paper", "analyzed_data", "conceived_experiments", "contributed_tools", "performed_experiments", "au_first", "au_middle", "au_last"),
+                            labels = c("Wrote paper", "Analyzed data", "Conceived experiments", "Contributed tools", "Performed experiments", "1st author", "2nd/middle author", "Last author")) +
+      scale_linetype_manual("References", values = c("solid", "solid", "solid", "solid", "solid", "dashed", "dashed", "dashed"),
+                            breaks = c("wrote_paper", "analyzed_data", "conceived_experiments", "contributed_tools", "performed_experiments", "au_first", "au_middle", "au_last"),
+                            labels = c("Wrote paper", "Analyzed data", "Conceived experiments", "Contributed tools", "Performed experiments", "1st author", "2nd/middle author", "Last author"))
+  })
+
+  output$contribution_position_number_authors <- renderPlot({
+    gather(data = df_grouped_number_authors[which(df_grouped_number_authors$n_authors >= input$number_authors[1] & df_grouped_number_authors$n_authors <= input$number_authors[2]),],
+           key, value, wrote_paper, analyzed_data, conceived_experiments, contributed_tools, performed_experiments, au_first, au_middle, au_last) %>%
+      
+      {if (!input$contribution_WR) filter(., key!='wrote_paper') else filter(.)} %>%
+      {if (!input$contribution_AD) filter(., key!='analyzed_data') else filter(.)} %>%
+      {if (!input$contribution_CE) filter(., key!='conceived_experiments') else filter(.)} %>%
+      {if (!input$contribution_CT) filter(., key!='contributed_tools') else filter(.)} %>%
+      {if (!input$contribution_PE) filter(., key!='performed_experiments') else filter(.)} %>%
+      {if (!input$position_first) filter(., key!='au_first') else filter(.)} %>%
+      {if (!input$position_middle) filter(., key!='au_middle') else filter(.)} %>%
+      {if (!input$position_last) filter(., key!='au_last') else filter(.)} %>%
+      
+      ggplot(aes(x = n_authors, y = value, colour = key, linetype = key)) +
+      geom_line() +
+      theme_minimal() +
+      xlab("Number of authors") +
       ylab("Count") +
       scale_colour_discrete("References", breaks = c("wrote_paper", "analyzed_data", "conceived_experiments", "contributed_tools", "performed_experiments", "au_first", "au_middle", "au_last"),
                             labels = c("Wrote paper", "Analyzed data", "Conceived experiments", "Contributed tools", "Performed experiments", "1st author", "2nd/middle author", "Last author")) +
